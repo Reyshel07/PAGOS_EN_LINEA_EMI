77eeab525ec7b4d2bb576cbeda1d34129b43e358
Reyshel07 <71579864jp@gmail.com>
2024-08-02 11:48:51 -0400

    commit17

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:PAGOS_EN_LINIA_EMI/main.dart';
+import 'package:pagos_en_linea_emi/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
